7a9a97d4a67464085f08ba2c3f18f96b5b0cc2c5
Peter Szabo <pts@fazekas.hu>
2022-05-27 23:00:31 +0200

    added dosmcdir.pl (build script with dosmc)

---
+++ dosmcdir.pl
@@ -0,0 +1,12 @@
+#
+# dosmclib/dosmc.pl: build script for mininasm with dosmc
+# by pts@fazekas.hu at Fri May 27 22:56:48 CEST 2022
+#
+# Usage: dosmc .
+# Output file: mininasm.com
+#
+
+BEGIN { $^W = 1 }
+dosmc(qw(-mt mininasm.c ins.c dosmclib.c bbprintf.c));  # Creates mininasm.com.
+my $size = -s("mininasm.com");
+print STDERR "info: created mininasm.com ($size bytes)\n";
