438302a14202a3941069dd6debebb046be1202bd
Peter Szabo <pts@fazekas.hu>
2022-05-27 23:00:03 +0200

    added dosmclib.c with dummy implementations of the missing libc functions

---
+++ dosmclib.c
@@ -0,0 +1,20 @@
+#include <dosmc.h>
+
+#include "dosmclib.h"
+
+_WCNORETURN static void unimplemented(const char *func) {
+  static const STRING_WITHOUT_NUL(msg, "fatal: function not implemented: \n");
+  write(2, msg, sizeof(msg) - 1);
+  write(2, func, strlen(func));
+  write(2, msg + sizeof(msg) - 1, 1);
+  exit(126);
+}
+
+FILE *stderr;
+void *malloc(size_t size) { (void)size; unimplemented("malloc"); }
+int fprintf(FILE *stream, const char *format, ...) { (void)stream; (void)format; unimplemented("fprintf"); }
+FILE *fopen(const char *path, const char *mode) { (void)path; (void)mode; unimplemented("fopen"); }
+size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream) { (void)ptr; (void)size; (void)nmemb; (void)stream; unimplemented("fread"); }
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) { (void)ptr; (void)size; (void)nmemb; (void)stream; unimplemented("fwrite"); }
+char *fgets(char *s, int size, FILE *stream) { (void)s; (void)size; (void)stream; unimplemented("fgets"); }
+int fclose(FILE *stream) { (void)stream; unimplemented("fclose"); }
