3e12f85a8dcb7f7736bf8ca3e5ed7bc9ecd0e227
Peter Szabo <pts@fazekas.hu>
2022-07-05 07:51:00 +0200

    bugfix: added test exit on assembler error

--- compat32_test.sh
+++ compat32_test.sh
@@ -3,17 +3,17 @@
 
 set -ex
 
-nasm-0.98.39.static -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-nasm-0.99.06.static -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
+nasm-0.98.39.static -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+nasm-0.99.06.static -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
 # Yasm is usually incompatible.
-#yasm-1.2.0 -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-#yasm-1.3.0 -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-nasm -w-number-overflow -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-./mininasm -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-./mininasm.gcc32 -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-./mininasm.tcc -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-./mininasm.tcc64 -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-"${KVIKDOS:-kvikdos}" mininasm.com -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
-"${KVIKDOS:-kvikdos}" mininasm.exe -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin
+#yasm-1.2.0 -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+#yasm-1.3.0 -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+nasm -w-number-overflow -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+./mininasm -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+./mininasm.gcc32 -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+./mininasm.tcc -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+./mininasm.tcc64 -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+"${KVIKDOS:-kvikdos}" mininasm.com -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
+"${KVIKDOS:-kvikdos}" mininasm.exe -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
 
 : "$0" OK.
