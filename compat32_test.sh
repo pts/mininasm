d13d84a6b7f6359f874e7d9876f825508e7e1289
Peter Szabo <pts@fazekas.hu>
2022-07-05 08:08:58 +0200

    added `./compat32_test.sh --compile' command-line flag

--- compat32_test.sh
+++ compat32_test.sh
@@ -13,6 +13,15 @@ fi
 
 set -ex
 
+if test "$1" = --compile; then
+gcc -ansi -pedantic -s -Os -W -Wall -Wno-overlength-strings -o mininasm mininasm.c bbprintf.c || exit "$?" #&& ls -ld mininasm
+gcc -m32 -ansi -pedantic -s -Os -W -Wall -Wno-overlength-strings -o mininasm.gcc32 mininasm.c bbprintf.c || exit "$?" #&& ls -ld mininasm.gcc32
+owcc -bdos -o mininasm.exe -mcmodel=c -Os -s -fstack-check -Wl,option -Wl,stack=1800 -march=i86 -W -Wall -Wextra mininasm.c bbprintf.c || exit "$?" #&& ls -ld mininasm.exe
+pts-tcc -s -O2 -W -Wall -o mininasm.tcc mininasm.c bbprintf.c || exit "$?" #&& ls -ld mininasm.tcc
+pts-tcc64 -m64 -s -O2 -W -Wall -o mininasm.tcc64 mininasm.c bbprintf.c || exit "$?" #&& ls -ld mininasm.tcc64
+"$DOSMC" -mt mininasm.c bbprintf.c || exit "$?"
+fi
+
 nasm-0.98.39.static -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
 nasm-0.99.06.static -f bin -o compat32.bin compat32.asm && cmp -l compat32.bin.good compat32.bin || exit "$?"
 # Yasm is usually incompatible.
