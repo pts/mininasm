876057a5630adff0c7ec2caecb9e1a4e091f2ed7
Peter Szabo <pts@fazekas.hu>
2022-11-24 23:22:42 +0100

    added bbprintf.h (it was a mistake to be omitted)

---
+++ bbprintf.h
@@ -0,0 +1,29 @@
+#ifndef _BBPRINTF_H_
+#define _BBPRINTF_H_ 1
+#pragma once
+
+#ifndef CONFIG_BBPRINTF_STATIC
+#define CONFIG_BBPRINTF_STATIC
+#endif
+
+struct bbprintf_buf {
+  char *buf, *buf_end, *p;
+  void *data;  /* Used by bbb.flush. */
+  void (*flush)(struct bbprintf_buf *bbb);
+};
+
+CONFIG_BBPRINTF_STATIC int bbprintf(struct bbprintf_buf *bbb, const char *format, ...);
+
+#if 0 /* Unused. */
+/* out must not be NULL. */
+CONFIG_BBPRINTF_STATIC int bbsprintf(char *out, const char *format, ...);
+#endif
+
+#if 0 /* Unused. */
+/* out must not be NULL. size must be >= 1. */
+CONFIG_BBPRINTF_STATIC int bbsnprintf(char *out, int size, const char *format, ...);
+#endif
+
+CONFIG_BBPRINTF_STATIC void bbwrite1(struct bbprintf_buf *bbb, int c);
+
+#endif  /* _BBPRINTF_H_ */
